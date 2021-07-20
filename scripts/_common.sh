7a41d35ffecc9b0508724106563333d7fd9d4792
ericgaspar <junk.eg@free.fr>
2021-07-20 18:00:29 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 YNH_PHP_VERSION="7.3"
-
+YNH_COMPOSER_VERSION=2.1.1
 extra_php_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-cli"
 
 #=================================================
