637754a12af0264130cc133c7c2704582648cc76
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-02-15 12:16:05 +0100

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -92,8 +92,8 @@ $config = [
         'auth' => true,                    // Enable SMTP authentication
         'username' => '__APP__',            // SMTP username
         'password' => '__MAIL_PWD__',       // SMTP password
-        'secure' => 'ssl',                  // Enable encryption (false, tls or ssl)
-        'port' => 465,                       // TCP port to connect to
+        'secure' => false,                  // Enable encryption (false, tls or ssl)
+        'port' => 25,                       // TCP port to connect to
     ],
     /* home */
     'show_what_is_that' => false,            // display "how to use" section
